57ff31a2c3580fab8d872650e54f963a45a40207
Glynn Quelch <glynn.quelch@gmail.com>
2022-05-27 22:00:25 +0100

    All dev dependecnies updated

--- tests/Fixtures/HTTP_TestCase.php
+++ tests/Fixtures/HTTP_TestCase.php
@@ -30,7 +30,8 @@ use PinkCrab\Loader\Hook_Loader;
 use PinkCrab\Route\Registration\WP_Rest_Registrar;
 use WP_REST_Response;
 
-abstract class HTTP_TestCase extends \WP_UnitTestCase {
+abstract class HTTP_TestCase extends \WP_UnitTestCase
+{
 
 	/** @var Spy_REST_Server */
 	protected $server;
@@ -38,9 +39,10 @@ abstract class HTTP_TestCase extends \WP_UnitTestCase {
 	/** @var Route_Manager */
 	protected $route_manager;
 
-	public function setUp() {
+	public function setUp(): void
+	{
 		parent::setUp();
-		add_filter( 'rest_url', array( $this, 'filter_rest_url_for_leading_slash' ), 10, 2 );
+		add_filter('rest_url', array($this, 'filter_rest_url_for_leading_slash'), 10, 2);
 
 		// Create route manager.
 		$this->route_manager = new Route_Manager(
@@ -54,25 +56,28 @@ abstract class HTTP_TestCase extends \WP_UnitTestCase {
 		$this->server   = $wp_rest_server;
 	}
 
-	public function tearDown() {
+	public function tearDown(): void
+	{
 		parent::tearDown();
-		remove_filter( 'rest_url', array( $this, 'test_rest_url_for_leading_slash' ), 10, 2 );
+		remove_filter('rest_url', array($this, 'test_rest_url_for_leading_slash'), 10, 2);
 		/** @var WP_REST_Server $wp_rest_server */
 		global $wp_rest_server;
 		$wp_rest_server = null;
 	}
 
-	public function register_routes(): void {
-		do_action( 'rest_api_init', $this->wp_rest_server );
+	public function register_routes(): void
+	{
+		do_action('rest_api_init', $this->wp_rest_server);
 	}
 
-	public function filter_rest_url_for_leading_slash( $url, $path ) {
-		if ( is_multisite() || get_option( 'permalink_structure' ) ) {
+	public function filter_rest_url_for_leading_slash($url, $path)
+	{
+		if (is_multisite() || get_option('permalink_structure')) {
 			return $url;
 		}
 
 		// Make sure path for rest_url has a leading slash for proper resolution.
-		$this->assertTrue( 0 === strpos( $path, '/' ), 'REST API URL should have a leading slash.' );
+		$this->assertTrue(0 === strpos($path, '/'), 'REST API URL should have a leading slash.');
 
 		return $url;
 	}
@@ -94,11 +99,11 @@ abstract class HTTP_TestCase extends \WP_UnitTestCase {
 		array $args = array(),
 		?callable $config = null
 	) {
-		$request = new \WP_REST_Request( $method, $route, $args );
-		if ( $config ) {
-			$request = $config( $request );
+		$request = new \WP_REST_Request($method, $route, $args);
+		if ($config) {
+			$request = $config($request);
 		}
-		return $this->server->dispatch( $request );
+		return $this->server->dispatch($request);
 	}
 
 	public function get_server(): \WP_REST_Server
--- tests/Unit/Registration/Test_Route_Manager.php
+++ tests/Unit/Registration/Test_Route_Manager.php
@@ -36,15 +36,17 @@ use PinkCrab\WP_Rest_Schema\Argument\String_Type;
 use PinkCrab\Route\Registration\WP_Rest_Registrar;
 use PinkCrab\WP_Rest_Schema\Argument\Boolean_Type;
 
-class Test_Route_Manager extends WP_UnitTestCase {
+class Test_Route_Manager extends WP_UnitTestCase
+{
 
 	/** @var Route_Manager */
 	protected $route_manager;
 
-	function setUp(): void {
+	function setUp(): void
+	{
 		parent::setUp();
 
-		$this->route_manager = new Route_Manager( new WP_Rest_Registrar(), new Hook_Loader() );
+		$this->route_manager = new Route_Manager(new WP_Rest_Registrar(), new Hook_Loader());
 	}
 
 	/**
@@ -52,103 +54,106 @@ class Test_Route_Manager extends WP_UnitTestCase {
 	 *
 	 * @return \PinkCrab\Route\Route\Route_Group
 	 */
-	public static function basic_group_provider(): Route_Group {
-		$group = new Route_Group( 'acme', '/route' );
-		$group->get( 'is_string' )
-			->argument( String_Type::on( 'id' ) )
-			->authentication( 'is_string' );
+	public static function basic_group_provider(): Route_Group
+	{
+		$group = new Route_Group('acme', '/route');
+		$group->get('is_string')
+			->argument(String_Type::on('id'))
+			->authentication('is_string');
 
-		$group->post( 'is_array' )
-			->authentication( 'is_array' );
+		$group->post('is_array')
+			->authentication('is_array');
 
-		$group->authentication( 'is_bool' );
-		$group->argument( Boolean_Type::on( 'id' ) );
+		$group->authentication('is_bool');
+		$group->argument(Boolean_Type::on('id'));
 		return $group;
 	}
 
 	/** @testdox It should be possible to create partially populated route based on the values set in the group. */
-	public function test_can_create_base_route_from_group(): void {
+	public function test_can_create_base_route_from_group(): void
+	{
 
 		$group = self::basic_group_provider();
 		$route = Objects::invoke_method(
 			$this->route_manager,
 			'create_base_route_from_group',
-			array( 'GET', $group )
+			array('GET', $group)
 		);
 
-		$this->assertEquals( '/route', $route->get_route() );
-		$this->assertEquals( 'acme', $route->get_namespace() );
-		$this->assertEquals( 'GET', $route->get_method() );
-		$this->assertTrue( $route->has_argument( 'id' ) );
-		$this->assertContains( 'is_bool', $route->get_authentication() );
+		$this->assertEquals('/route', $route->get_route());
+		$this->assertEquals('acme', $route->get_namespace());
+		$this->assertEquals('GET', $route->get_method());
+		$this->assertTrue($route->has_argument('id'));
+		$this->assertContains('is_bool', $route->get_authentication());
 	}
 
 	/** @testdox When a group is registered, each route within the group should be populate each route using the base values from the group.  */
-	public function test_can_unpack_group() :void {
+	public function test_can_unpack_group(): void
+	{
 		$group  = self::basic_group_provider();
 		$routes = Objects::invoke_method(
 			$this->route_manager,
 			'unpack_group',
-			array( $group )
+			array($group)
 		);
 
-		$this->assertIsArray( $routes );
-		$this->assertArrayHasKey( 'GET', $routes );
-		$this->assertArrayHasKey( 'POST', $routes );
+		$this->assertIsArray($routes);
+		$this->assertArrayHasKey('GET', $routes);
+		$this->assertArrayHasKey('POST', $routes);
 
 		// Check the get, should have replaced the argument for ID and stacked auth callbacks
 		$get = $routes['GET'];
 
-		$this->assertEquals( '/route', $get->get_route() );
-		$this->assertEquals( 'acme', $get->get_namespace() );
-		$this->assertEquals( 'GET', $get->get_method() );
+		$this->assertEquals('/route', $get->get_route());
+		$this->assertEquals('acme', $get->get_namespace());
+		$this->assertEquals('GET', $get->get_method());
 
-		$this->assertCount( 2, $get->get_authentication() );
-		$this->assertContains( 'is_bool', $get->get_authentication() );
-		$this->assertContains( 'is_string', $get->get_authentication() );
+		$this->assertCount(2, $get->get_authentication());
+		$this->assertContains('is_bool', $get->get_authentication());
+		$this->assertContains('is_string', $get->get_authentication());
 
-		$this->assertTrue( $get->has_argument( 'id' ) );
+		$this->assertTrue($get->has_argument('id'));
 		$argument = $get->get_arguments();
-		$this->assertEquals( 'id', $argument['id']->get_key() );
-		$this->assertEquals( 'string', $argument['id']->get_type() ); // As per route definition.
+		$this->assertEquals('id', $argument['id']->get_key());
+		$this->assertEquals('string', $argument['id']->get_type()); // As per route definition.
 
 		// Check the post, should have NOT replaced the argument for ID and stacked auth callbacks
 		$post = $routes['POST'];
 
-		$this->assertEquals( '/route', $post->get_route() );
-		$this->assertEquals( 'acme', $post->get_namespace() );
-		$this->assertEquals( 'POST', $post->get_method() );
+		$this->assertEquals('/route', $post->get_route());
+		$this->assertEquals('acme', $post->get_namespace());
+		$this->assertEquals('POST', $post->get_method());
 
-		$this->assertCount( 2, $post->get_authentication() );
-		$this->assertContains( 'is_bool', $post->get_authentication() );
-		$this->assertContains( 'is_array', $post->get_authentication() );
+		$this->assertCount(2, $post->get_authentication());
+		$this->assertContains('is_bool', $post->get_authentication());
+		$this->assertContains('is_array', $post->get_authentication());
 
-		$this->assertTrue( $post->has_argument( 'id' ) );
+		$this->assertTrue($post->has_argument('id'));
 		$argument = $post->get_arguments();
-		$this->assertEquals( 'id', $argument['id']->get_key() );
-		$this->assertEquals( 'boolean', $argument['id']->get_type() ); // As per group definition
+		$this->assertEquals('id', $argument['id']->get_key());
+		$this->assertEquals('boolean', $argument['id']->get_type()); // As per group definition
 	}
 
 	/** @testdox If a group is created that has a route/method with no callback defined, an eror should be thrown and the registration process ended. */
-	public function test_throws_exception_if_route_has_no_callback(): void {
+	public function test_throws_exception_if_route_has_no_callback(): void
+	{
 
-		$this->expectException( Route_Exception::class );
-		$this->expectExceptionCode( 102 );
+		$this->expectException(Route_Exception::class);
+		$this->expectExceptionCode(102);
 
 		// Create a group with a route that has no callback.
-		$group = new Route_Group( 'namespace', 'no-callback' );
+		$group = new Route_Group('namespace', 'no-callback');
 		Objects::set_property(
 			$group,
 			'routes',
-			array( 'GET' => new Route( 'GET', 'ignore' ) )
+			array('GET' => new Route('GET', 'ignore'))
 		);
 
 		// Attempt to unpack
 		Objects::invoke_method(
 			$this->route_manager,
 			'unpack_group',
-			array( $group )
+			array($group)
 		);
-
 	}
 }
