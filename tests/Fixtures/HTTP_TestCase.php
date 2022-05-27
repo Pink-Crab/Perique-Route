23daab78abd9d339dd7488feace46276e473f347
Glynn Quelch <glynn.quelch@gmail.com>
2022-05-27 22:02:29 +0100

    House Keeping

--- tests/Fixtures/HTTP_TestCase.php
+++ tests/Fixtures/HTTP_TestCase.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 /**
  * Base class for Rest Integration tests.
- *  
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -30,8 +30,8 @@ use PinkCrab\Loader\Hook_Loader;
 use PinkCrab\Route\Registration\WP_Rest_Registrar;
 use WP_REST_Response;
 
-abstract class HTTP_TestCase extends \WP_UnitTestCase
-{
+abstract class HTTP_TestCase extends \WP_UnitTestCase {
+
 
 	/** @var Spy_REST_Server */
 	protected $server;
@@ -39,45 +39,41 @@ abstract class HTTP_TestCase extends \WP_UnitTestCase
 	/** @var Route_Manager */
 	protected $route_manager;
 
-	public function setUp(): void
-	{
+	public function setUp(): void {
 		parent::setUp();
-		add_filter('rest_url', array($this, 'filter_rest_url_for_leading_slash'), 10, 2);
+		add_filter( 'rest_url', array( $this, 'filter_rest_url_for_leading_slash' ), 10, 2 );
 
 		// Create route manager.
 		$this->route_manager = new Route_Manager(
 			new WP_Rest_Registrar(),
-			new Hook_Loader
+			new Hook_Loader()
 		);
 
 		/** @var WP_REST_Server $wp_rest_server */
 		global $wp_rest_server;
-		$wp_rest_server = new \Spy_REST_Server;
+		$wp_rest_server = new \Spy_REST_Server();
 		$this->server   = $wp_rest_server;
 	}
 
-	public function tearDown(): void
-	{
+	public function tearDown(): void {
 		parent::tearDown();
-		remove_filter('rest_url', array($this, 'test_rest_url_for_leading_slash'), 10, 2);
+		remove_filter( 'rest_url', array( $this, 'test_rest_url_for_leading_slash' ), 10, 2 );
 		/** @var WP_REST_Server $wp_rest_server */
 		global $wp_rest_server;
 		$wp_rest_server = null;
 	}
 
-	public function register_routes(): void
-	{
-		do_action('rest_api_init', $this->wp_rest_server);
+	public function register_routes(): void {
+		do_action( 'rest_api_init', $this->wp_rest_server );
 	}
 
-	public function filter_rest_url_for_leading_slash($url, $path)
-	{
-		if (is_multisite() || get_option('permalink_structure')) {
+	public function filter_rest_url_for_leading_slash( $url, $path ) {
+		if ( is_multisite() || get_option( 'permalink_structure' ) ) {
 			return $url;
 		}
 
 		// Make sure path for rest_url has a leading slash for proper resolution.
-		$this->assertTrue(0 === strpos($path, '/'), 'REST API URL should have a leading slash.');
+		$this->assertTrue( 0 === strpos( $path, '/' ), 'REST API URL should have a leading slash.' );
 
 		return $url;
 	}
@@ -99,15 +95,14 @@ abstract class HTTP_TestCase extends \WP_UnitTestCase
 		array $args = array(),
 		?callable $config = null
 	) {
-		$request = new \WP_REST_Request($method, $route, $args);
-		if ($config) {
-			$request = $config($request);
+		$request = new \WP_REST_Request( $method, $route, $args );
+		if ( $config ) {
+			$request = $config( $request );
 		}
-		return $this->server->dispatch($request);
+		return $this->server->dispatch( $request );
 	}
 
-	public function get_server(): \WP_REST_Server
-	{
+	public function get_server(): \WP_REST_Server {
 		return $this->server;
 	}
 }
